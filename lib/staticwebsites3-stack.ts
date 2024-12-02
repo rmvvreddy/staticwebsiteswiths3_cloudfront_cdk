636480e17f592a9a86eecc00a5c2a112ed380fc1
Ratnam Solutions <manikanta@ratnamsolutions.com>
2024-12-02 17:58:57 +0530

    static website with cloud front

--- lib/staticwebsites3-stack.ts
+++ lib/staticwebsites3-stack.ts
@@ -1,16 +1,90 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+import * as iam from 'aws-cdk-lib/aws-iam';
+
 
 export class Staticwebsites3Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const siteBucket = new s3.Bucket(this, 'SiteBucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // Use RETAIN for production
+      autoDeleteObjects: true, // For testing; disable for production
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'Staticwebsites3Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+   
+    const originAccessControl = new cloudfront.CfnOriginAccessControl(this, 'OAC', {
+      originAccessControlConfig: {
+        name: 'WebsiteOAC',
+        signingBehavior: 'always', // Always sign requests
+        originAccessControlOriginType: 's3', // Must match the S3 origin type
+        signingProtocol: 'sigv4',
+      },
+    });
+
+    // cloudfront
+    const distribution = new cloudfront.CfnDistribution(this, 'SiteDistribution', {
+      distributionConfig: {
+        enabled: true,
+        origins: [
+          {
+            id: 'S3Origin',
+            domainName: siteBucket.bucketRegionalDomainName,
+            originAccessControlId: originAccessControl.attrId, // Attach the OAC
+            s3OriginConfig: {}, // Required for S3 origin with OAC
+          },
+        ],
+        defaultCacheBehavior: {
+          targetOriginId: 'S3Origin',
+          viewerProtocolPolicy: 'redirect-to-https',
+          allowedMethods: ['GET', 'HEAD'],
+          cachedMethods: ['GET', 'HEAD'],
+          forwardedValues: {
+            queryString: false,
+            cookies: { forward: 'none' },
+          },
+        },
+        defaultRootObject: 'index.html',
+      },
+    });
+
+    //  permissions to the S3 bucket
+    siteBucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        actions: ['s3:GetObject'],
+        resources: [siteBucket.arnForObjects('*')],
+        principals: [
+          new iam.ServicePrincipal('cloudfront.amazonaws.com', {
+            conditions: {
+              StringEquals: {
+                'AWS:SourceArn': `arn:aws:cloudfront::${this.account}:distribution/${distribution.ref}`,
+              },
+            },
+          }),
+        ],
+      })
+    );
+
+    // Add BucketDeployment to deploy static files and invalidate cache
+    new s3deploy.BucketDeployment(this, 'DeployWithInvalidation', {
+      sources: [s3deploy.Source.asset('./appcode')], // Path to your static website files
+      destinationBucket: siteBucket,
+      distribution: cloudfront.Distribution.fromDistributionAttributes(this, 'DistributionRef', {
+        distributionId: distribution.ref,
+        domainName: distribution.attrDomainName,
+      }),
+      distributionPaths: ['/*'], // Invalidate all paths
+    });
+
+    // Output the CloudFront URL
+    new cdk.CfnOutput(this, 'CloudFrontURL', {
+      value: `https://${distribution.attrDomainName}`,
+      description: 'The CloudFront Distribution URL for the website',
+    });
   }
 }
